389b877bdb05db7d5710460d377936df8d7cca8e
Oignnon - <td665pro@pm.me>
2023-03-08 13:08:38 +0100

    Front end of site working

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a5ef2ffd9aff023ca2fad460ce0b297a&page=1";
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=a5ef2ffd9aff023ca2fad460ce0b297a&query";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=a5ef2ffd9aff023ca2fad460ce0b297a&query=";
 
 //Send query to API and retrieve it.
 
@@ -9,17 +9,29 @@ const form = document.getElementById("form");
 const search = document.getElementById("query");
 
 //Fetch from URL
+returnMovies(APILINK)  //<<<< Didn't work without this
 function returnMovies(url){
     fetch(url).then(res => res.json())
-    .then(function(data)){
+    .then(function(data){
         console.log(data.results);
         data.results.forEach(element => {
             const div_card = document.createElement('div');
             //create new div element!^^
+            div_card.setAttribute('class', 'card');
+            
             const div_row = document.createElement('div');
+            div_row.setAttribute('class', 'row');
+
             const div_column = document.createElement('div');
+            div_column.setAttribute('class', 'column');
+
             const image = document.createElement('img');
+            image.setAttribute('class', 'thumbnail');
+            image.setAttribute('id', 'image');
+
             const title = document.createElement('h3');
+            title.setAttribute('id', 'title');
+
             const center = document.createElement('center');
 
             title.innerHTML = `${element.title}`;
@@ -39,14 +51,14 @@ function returnMovies(url){
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    main.innerHTML = "";
+    main.innerHTML = '';
     //^^ Remove all initial movies after search
 
     const searchItem = search.value;
 
     if (searchItem) {
         returnMovies(SEARCHAPI + searchItem);
-        search.value = "";
+        search.value = '';
     }
 
 });
