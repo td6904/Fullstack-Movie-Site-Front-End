fa986f8ec0a04b3be04ea146f93519bdf5417ade
Oignnon - <td665pro@pm.me>
2023-03-15 16:21:24 +0100

    Remove tracked files

--- Front_end/script.js
+++ Front_end/script.js
@@ -1,8 +1,4 @@
-const APILINK = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a5ef2ffd9aff023ca2fad460ce0b297a&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=a5ef2ffd9aff023ca2fad460ce0b297a&query=";
 
-//Send query to API and retrieve it.
 
 const main = document.getElementById("section");
 const form = document.getElementById("form");
